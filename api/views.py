6d75bf873a44f8f129fc4ad45609891e71141850
Nam Nguyen <sworduser.no2@gmail.com>
2018-07-20 04:20:19 -0700

    update signup api

--- api/views.py
+++ api/views.py
@@ -125,7 +125,7 @@ class VerifyAccount(APIView):
                     else:
                         return Response({'status':"code_incorrect"}, status=status.HTTP_200_OK)
                 else:
-                    return Response({'status':"missing_params"}, status=701)
+                    return Response({'status':"missing_params"}, status=400)
             else:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
